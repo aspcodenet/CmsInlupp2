99f8d4b6876c6fd77cface1892329f12189151d0
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-09-22 14:59:16 +0200

    Add project files.

---
+++ CmsInlupp2/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace CmsInlupp2
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+
+        public decimal Price { get; set; }
+
+        //TODO: Fråga Stefan det där med Enum
+        public string Typ { get; set; }
+    }
+}
---
+++ CmsInlupp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+
+namespace CmsInlupp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allProducts = ReadProductsFromFile();
+
+            while (true)
+            {
+                Console.WriteLine("1. Ny kund");
+                Console.WriteLine("0. Avsluta");
+                Console.WriteLine("Mata in nåt:");
+                var sel = Console.ReadLine();
+                if (sel == "0")
+                    break;
+                if (sel == "1")
+                    NyKund();
+            }
+        }
+
+        private static List<Product> ReadProductsFromFile()
+        {
+            var players = new List<Product>();
+
+            using (var sr =
+                File.OpenText(@"..\..\..\ProduktDatabasen.txt"))
+
+            {
+                while (true)
+                {
+                    var line = sr.ReadLine();
+                    if (line == null) break;
+                    //Bananer,300,12,typ
+                    string[] split = line.Split(',');
+                    var p = new Product();
+                    p.Name = split[0];
+                    p.ProductId = Convert.ToInt32(split[1]);
+                    p.Price = Convert.ToDecimal(split[2]);
+                    p.Typ = split[3];
+                    players.Add(p);
+                }
+
+            }
+
+            return players;
+        }
+        private static void NyKund()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
